2398335529fc741d6ad5d20321fb7701e7c16464
ozaki25 <fs65ugumori@yahoo.co.jp>
2019-06-26 17:05:27 +0900

    add story

---
+++ src/components/MyButton.stories.js
@@ -0,0 +1,51 @@
+import { storiesOf } from "@storybook/vue";
+import { action } from "@storybook/addon-actions";
+import { boolean, text } from "@storybook/addon-knobs";
+import MyButton from "./MyButton.vue";
+
+const story = storiesOf("MyButton", module);
+
+story.add("nomal", () => ({
+  components: { MyButton },
+  props: {
+    text: {
+      default: text("Text", "Hello")
+    },
+    disabled: {
+      default: boolean("disabled", false)
+    }
+  },
+  methods: {
+    onClick: action("on click")
+  },
+  template: `<MyButton :onClick="onClick" :disabled="disabled" >{{text}}</MyButton>`
+}));
+
+story.add("primary", () => ({
+  components: { MyButton },
+  props: {
+    text: {
+      default: text("Text", "Hello")
+    },
+    disabled: {
+      default: boolean("disabled", false)
+    }
+  },
+  methods: {
+    onClick: action("on click")
+  },
+  template: `<MyButton :onClick="onClick" :disabled="disabled" primary>{{text}}</MyButton>`
+}));
+
+story.add("disabled", () => ({
+  components: { MyButton },
+  props: {
+    text: {
+      default: text("Text", "Hello")
+    }
+  },
+  methods: {
+    onClick: action("on click")
+  },
+  template: `<MyButton @click.native="onClick" disabled>{{text}}</MyButton>`
+}));
