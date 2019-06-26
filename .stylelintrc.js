9574740764cc911d937f1821b73ec66a9529b88b
ozaki <fs65ugumori@yahoo.co.jp>
2019-06-27 08:25:11 +0900

    add sass stylelint

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -1,4 +1,8 @@
 module.exports = {
   root: true,
-  'extends': 'stylelint-config-standard'
-}
+  extends: [
+    'stylelint-config-recommended-scss',
+    'stylelint-config-rational-order',
+    './node_modules/prettier-stylelint/config.js',
+  ],
+};
