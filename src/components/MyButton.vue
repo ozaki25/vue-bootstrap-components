c9128aa05580be2dd3b63c3bf05a80dec0b6c8cf
ozaki <fs65ugumori@yahoo.co.jp>
2019-06-27 08:31:59 +0900

    apply stylelint

--- src/components/MyButton.vue
+++ src/components/MyButton.vue
@@ -31,20 +31,37 @@ export default {
 <style lang="scss" scoped>
 button {
   display: inline-block;
+  margin: 0;
   padding: 6px 12px;
-  margin-bottom: 0;
-  font-size: 14px;
+  overflow: visible;
   font-weight: normal;
-  text-align: center;
-  vertical-align: middle;
+  font-size: 14px;
+  line-height: 1.42857143;
   white-space: nowrap;
+  text-align: center;
+  text-transform: none;
+  vertical-align: middle;
+  background-image: none;
   border: 1px solid transparent;
   border-radius: 4px;
   cursor: pointer;
+  -webkit-appearance: button;
+  -webkit-user-select: none;
+  -moz-user-select: none;
+  -ms-user-select: none;
+  user-select: none;
+  -ms-touch-action: manipulation;
+  touch-action: manipulation;
+
+  &:focus,
+  &:active {
+    outline: 5px auto -webkit-focus-ring-color;
+    outline-offset: -2px;
+  }
 }
 button[disabled] {
   cursor: not-allowed;
-  opacity: .65;
+  opacity: 0.65;
   filter: alpha(opacity=65);
   pointer-events: none;
 }
