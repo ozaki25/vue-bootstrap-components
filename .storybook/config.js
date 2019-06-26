0d8137c169547b0ee86fa4683119958980c49762
ozaki25 <fs65ugumori@yahoo.co.jp>
2019-06-26 17:00:58 +0900

    update storybook config

--- .storybook/config.js
+++ .storybook/config.js
@@ -3,8 +3,7 @@ import { withKnobs } from '@storybook/addon-knobs';
 
 addDecorator(withKnobs);
 
-// automatically import all files ending in *.stories.js
-const req = require.context('../stories', true, /\.stories\.js$/);
+const req = require.context('../src/components/', true, /\.stories\.js$/);
 function loadStories() {
   req.keys().forEach(filename => req(filename));
 }
