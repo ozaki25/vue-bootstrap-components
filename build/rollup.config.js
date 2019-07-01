1f44af52c3ed7c701555fd290e553a28bcfec11b
ozaki <fs65ugumori@yahoo.co.jp>
2019-07-01 10:05:15 +0900

    add minify config

--- build/rollup.config.js
+++ build/rollup.config.js
@@ -1,11 +1,18 @@
 import vue from "rollup-plugin-vue";
 import commonjs from "rollup-plugin-commonjs";
+import { terser } from "rollup-plugin-terser";
 
 export default {
   input: "src/index.js",
   output: {
-    file: "dist/my-component.js",
+    file: "dist/my-component.min.js",
     format: "esm"
   },
-  plugins: [vue({ css: true }), commonjs()]
+  plugins: [
+    vue({
+      css: true
+    }),
+    commonjs(),
+    terser()
+  ]
 };
