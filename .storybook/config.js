e2d09bc5125f962ea6a05bbe963aea4af6406fec
ozaki <fs65ugumori@yahoo.co.jp>
2019-07-23 08:14:51 +0900

    add storybook-addon-vue-info config

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,7 +1,9 @@
 import { addDecorator, configure } from '@storybook/vue';
 import { withKnobs } from '@storybook/addon-knobs';
+import { withInfo } from 'storybook-addon-vue-info';
 
 addDecorator(withKnobs);
+addDecorator(withInfo);
 
 const req = require.context('../src/components/', true, /\.stories\.js$/);
 function loadStories() {
